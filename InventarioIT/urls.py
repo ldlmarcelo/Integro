1b6f91a9ec7ec90f66da674d432b0382579df343
Marcelo Luna <ldl_marcelo@hotmail.com>
2025-02-23 17:46:05 -0300

    cambio de estrategia, todo desde el panel admin

--- InventarioIT/urls.py
+++ InventarioIT/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from inventario import views  # ¡Importante: importar las vistas de la APP inventario para el dashboard!
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('inventario/', include('inventario.urls')),  # ¡Incluye las URLs de la APP inventario BAJO el prefijo /inventario/ !
-    path('', views.dashboard_view, name='home'),  # URL para la raíz (Dashboard) - ¡MANTENER ESTA LÍNEA!
-]
+    ]
--- inventario/urls.py
+++ inventario/urls.py
@@ -1,23 +1,8 @@
 from django.urls import path
 from . import views
 
-app_name = 'inventario' # ¡Asegúrate de tener esta línea!
+app_name = 'inventario' 
 
 urlpatterns = [
-    path('', views.dashboard_view, name='home'),  
-
-    # URLs para Dispositivo (Vistas basadas en clase - CBV)
-    path('dispositivos/', views.DispositivoListView.as_view(), name='dispositivo_list'),
-    path('dispositivos/nuevo/', views.DispositivoCreateView.as_view(), name='dispositivo_new'),
-    path('dispositivos/editar/<int:pk>/', views.DispositivoUpdateView.as_view(), name='dispositivo_edit'),
-    path('dispositivos/borrar/<int:pk>/', views.DispositivoDeleteView.as_view(), name='dispositivo_delete'),
-
-    # URLs para TipoDispositivo (Vistas basadas en clase - CBV)
-    path('tipos-dispositivo/', views.TipoDispositivoListView.as_view(), name='tipodispositivo_list'),
-    path('tipos-dispositivo/nuevo/', views.TipoDispositivoCreateView.as_view(), name='tipodispositivo_new'),
-    path('tipos-dispositivo/editar/<int:pk>/', views.TipoDispositivoUpdateView.as_view(), name='tipodispositivo_edit'),
-    path('tipos-dispositivo/borrar/<int:pk>/', views.TipoDispositivoDeleteView.as_view(), name='tipodispositivo_delete'),
-
-
-
+ path('', views.dashboard_view, name='home'),
 ]
